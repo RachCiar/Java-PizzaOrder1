150963aa9c316c6cb74c259fe794d3f8ca644738
rciarl8967 <rciarl8967@wsacslt37208.suscorp.edu>
2019-04-25 14:38:23 -0400

    04252019-Java Pizza Order 1

---
+++ src/pizzaorder1/order.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pizzaorder1;
+
+/**
+ *
+ * @author rachc
+ */
+class order {
+
+    static void setCost(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
